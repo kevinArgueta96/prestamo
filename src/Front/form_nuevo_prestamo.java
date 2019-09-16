e727d0818faa11c79166ff056776c8910e68d21c
Kevin Argueta <kev200050@gmail.com>
2019-09-16 16:20:29 -0600

    cambios

--- src/Front/form_nuevo_prestamo.java
+++ src/Front/form_nuevo_prestamo.java
@@ -393,7 +393,7 @@ public class form_nuevo_prestamo extends javax.swing.JFrame {
     private void tbla_clieMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbla_clieMouseClicked
         int seleccion = tbla_clie.rowAtPoint(evt.getPoint());
         txt_id.setText(String.valueOf(tbla_clie.getValueAt(seleccion, 0)));
-        txt_dpi.setText(String.valueOf(tbla_clie.getValueAt(seleccion, 4)));
+        txt_dpi.setText(String.valueOf(tbla_clie.getValueAt(seleccion, 3)));
         txt_nombre.setText(String.valueOf(tbla_clie.getValueAt(seleccion, 1)));
     }//GEN-LAST:event_tbla_clieMouseClicked
     private void filtro(String consulta, JTable jtableBuscar) {
@@ -417,7 +417,7 @@ public class form_nuevo_prestamo extends javax.swing.JFrame {
         conexcion con = new conexcion();
         String query = "INSERT INTO tbl_prestamo (id_prestamo,monto,interes,fecha_creacion,fecha_finalizacion,monto_interes,ganancia,estado_garantia,"
                 + "cuotas,total_cuota"
-                + ",id_cliente,id_plazo,estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)";
+                + ",id_cliente,id_plazo,estado,saldo_faltante,cuota_faltante) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?,?)";
         String query2 = "SELECT id_cliente,estado FROM tbl_prestamo";
         String[] dato = new String[2];
         Statement str1;
@@ -488,6 +488,8 @@ public class form_nuevo_prestamo extends javax.swing.JFrame {
                                         str.setInt(11, id_cliente);
                                         str.setInt(12, seleccion);
                                         str.setInt(13, 1);
+                                        str.setDouble(14, monto_a_pa);
+                                        str.setInt(15, cuota);
 
                                         res = str.executeUpdate();
                                         if (res > 0) {
